e3d14674749aae03a9a24b0f796ca5ed48b2ba9f
xjccc <546534045@qq.com>
2023-09-18 15:18:10 +0800

    创建项目

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    AutoImport({
+      imports: ['vue', 'vue-router']
+    }),
+
     vue(),
   ],
   resolve: {
